4b5ca4f034b5b0be9f1a098bc0eca0360002a7f7
Sahil <g.sahil@iitg.ac.in>
2022-06-19 22:31:35 +0530

    adding files

---
+++ app.py
@@ -0,0 +1,246 @@
+import dash
+from dash import dcc
+from dash import html
+from datetime import datetime as dt
+import yfinance as yf
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
+# model
+from model import prediction
+from sklearn.svm import SVR
+
+def get_stock_price_fig(df):
+    fig = px.line(df, x = "Date", y = ["Close", "Open"], title="Closing and Opening Price vs Date")
+    return fig
+
+
+#Exponential weighted functions in Pandas
+#The ewm() function is used to provide exponential weighted functions.
+def get_more(df):
+    df['EWA_20'] = df['Close'].ewm(span = 20, adjust = False).mean()
+    fig = px.scatter(df, x = "Date",y = "EWA_20", title="Exponential Moving Average vs Date")
+    fig.update_traces(mode = 'lines+markers')
+    return fig
+
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[
+        "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+    ])
+server = app.server
+
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                html.P(
+                    "Welcome to StockDash App!",
+                    className="start"
+                ),
+                html.Div(
+                    [
+                        #stock code input
+                        html.P("Input stock code: "),
+                        html.Div([
+                            dcc.Input(
+                                id = "stock-code",
+                                placeholder='Enter a value...',
+                                type='text'
+                            ),
+                            html.Button("Submit", id='submit')
+                        ], className="stock-code-form"
+                        ),
+                        
+                    ], className="input"
+                ),
+                html.Div(
+                    [
+                        #Date range picker input
+                        dcc.DatePickerRange(
+                            id='date-picker-range',
+                            min_date_allowed=dt(1995, 8, 5),
+                            max_date_allowed=dt.now(),
+                            initial_visible_month=dt.now(),
+                            end_date=dt.now().date()
+                            
+                    )
+                    ], className="date"
+                ),
+                html.Div(
+                    [
+                        # Stock price button
+                        html.Button("Stock Price", className="stock-btn", id = "stock"),
+                        # Indicators button
+                        html.Button("Indicators", className="indicator-btn", id = "indicator"),
+                        # Number of days of forecast input
+                        dcc.Input(
+                            id = "n-days",
+                            placeholder='Number of days',
+                            type='text'
+                        ),
+                        # Forecast button
+                        html.Button("Forecast Button", className="forecast-btn", id="forecast")
+                    ], className="buttons"
+                )
+            ], className="navbar"
+        ),
+        #content
+        html.Div(
+            [
+                html.Div(
+                    [
+                        #Logo
+                        html.Img(id = "logo"),
+                        #Company name
+                        html.P(id = "ticker")
+                    ], className="header"
+                ),
+                html.Div(
+                    #Description
+                    id = "description",
+                    className="description-tracker"
+                ),
+                html.Div(
+                    [
+                        #Graph Content
+                    ],
+                    id="graph-content"
+                ),
+                html.Div(
+                    [
+                        #Indicator plot
+                    ],
+                    id = "main-content"
+                ),
+                html.Div(
+                    [
+                        #Forecast-plot
+                    ],
+                    id = "forecast-content"
+                )
+            ],className="content"
+        )
+        
+    ], className="container"
+)
+#callback for company info
+@app.callback(
+    [
+        Output("description", "children"),
+        Output("logo", "src"),
+        Output("ticker", "children"),
+        Output("stock", "n_clicks"),
+        Output("indicator", "n_clicks"),
+        Output("forecast", "n_clicks")
+    ],
+    [
+        Input("submit", "n_clicks")
+    ],
+    [
+        State("stock-code", "value")
+    ]
+    
+    
+)
+def update_data(n, val):
+    if n == None:
+        return "Hey there! Please enter a legitimate stock code to get details.", "https://m.foolcdn.com/media/dubs/original_images/Intro_slide_-_digital_stock_chart_going_up_-_source_getty.jpg", "Stonks", None, None, None
+    else:
+        if val == None:
+            raise PreventUpdate
+        else:
+            ticker = yf.Ticker(val)
+            inf = ticker.info 
+            df = pd.DataFrame().from_dict(inf, orient="index").T
+            df[['logo_url', 'shortName', 'longBusinessSummary']]
+            return df['longBusinessSummary'].values[0], df['logo_url'].values[0], df[ 'shortName'].values[0], None, None, None
+
+#State allows you to pass along extra values without firing the callbacks.
+
+#callback for stocks graphs
+@app.callback(
+    [
+        Output("graph-content","children")
+    ],
+    [
+        Input("stock", "n_clicks"),
+        Input('date-picker-range', 'start_date'),
+        Input('date-picker-range', 'end_date')
+    ],
+    [
+        State("stock-code", "value")
+    ]
+)
+def stock_price(n, start_date, end_date, val):
+    if n == None:
+        return [""]
+    if val == None:
+        raise PreventUpdate
+    else:
+        if start_date != None:
+            df = yf.download(val, str(start_date), str(end_date))
+        else:
+            df = yf.download(val)
+
+    df.reset_index(inplace = True)
+    fig = get_stock_price_fig(df)
+    return [dcc.Graph(figure = fig)]
+
+
+#callback for indicators
+
+@app.callback(
+    [
+        Output("main-content","children")
+    ],
+    [
+        Input("indicator", "n_clicks"),
+        Input('date-picker-range', 'start_date'),
+        Input('date-picker-range', 'end_date')
+    ],
+    [
+        State("stock-code", "value")
+    ]
+)
+def indicators(n , start_date, end_date, val):
+    if n == None:
+        return [""]
+    if val == None:
+        return [""]
+    else:
+        if start_date != None:
+            df = yf.download(val, str(start_date), str(end_date))
+        else:
+            df = yf.download(val)
+
+    df.reset_index(inplace = True)
+    fig = get_more(df)
+    return [dcc.Graph(figure = fig)]
+
+#callback for forecast
+@app.callback(
+    [
+        Output("forecast-content","children")
+    ],
+    [
+        Input("forecast", "n_clicks")
+    ],
+    [
+        State("n-days", "value"),
+        State("stock-code", "value")
+    ]
+)
+def forecast(n, n_days, val):
+    if n == None:
+        return [""]
+    if val == None:
+        raise PreventUpdate
+    fig = prediction(val, int(n_days) + 1)
+    return [dcc.Graph(figure = fig)]
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ model.py
@@ -0,0 +1,107 @@
+from pickle import TRUE
+from tkinter.tix import X_REGION
+from typing import List
+
+
+def prediction(stock, n_days):
+    import dash
+    from dash import dcc
+    from dash import html
+    from datetime import datetime as dt
+    import yfinance as yf
+    from dash.dependencies import Input, Output, State
+    from dash.exceptions import PreventUpdate
+    import pandas as pd
+    import plotly.graph_objs as go
+    import plotly.express as px
+
+    #model
+    import numpy as np
+    from datetime import date, timedelta
+    from model import prediction
+    from sklearn.model_selection import train_test_split
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.svm import SVR
+
+
+    #load the data
+    df = yf.download(stock, period='60d')
+    df.reset_index(inplace = True)
+    df['Day'] = df.index
+
+    days = list()
+    for i in range(len(df.Day)):
+        days.append([i])
+
+    #Splitting the dataset
+    
+    X = days
+    Y = df[['Close']]
+
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+    gsc = GridSearchCV(
+        estimator=SVR(kernel='rbf'),
+        param_grid={
+            'C': [0.1, 1, 100, 1000],
+            'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
+            'gamma': [0.0001, 0.001, 0.005, 0.1, 1, 3, 5]
+        },
+        cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+        
+    y_train = y_train.values.ravel()
+    y_train
+    grid_result = gsc.fit(x_train, y_train)
+    best_params = grid_result.best_params_
+    best_svr = SVR(kernel='rbf',C=best_params["C"], epsilon=best_params["epsilon"], gamma=best_params["gamma"], max_iter=-1)
+    # best_svr.fit(x_train, y_train)
+    # svm_confidence = best_svr.score(x_train, y_train)
+    # print(svm_confidence)
+
+    # Support Vector Regression Models
+
+    # RBF model
+    #rbf_svr = SVR(kernel='rbf', C=1000.0, gamma=4.0)
+
+    rbf_svr = best_svr.fit(x_train, y_train)
+    output_days = list()
+    for i in range(1, n_days):
+        output_days.append([i + x_test[-1][0]])
+
+    dates = []
+    current = date.today()
+    for i in range(n_days):
+        current += timedelta(days = 1)
+        dates.append(current)
+
+
+    # plot Results
+    # fig = go.Figure()
+    # fig.add_trace(
+    #     go.Scatter(x=np.array(x_test).flatten(),
+    #                y=y_test.values.flatten(),
+    #                mode='markers',
+    #                name='data'))
+    # fig.add_trace(
+    #     go.Scatter(x=np.array(x_test).flatten(),
+    #                y=rbf_svr.predict(x_test),
+    #                mode='lines+markers',
+    #                name='test'))
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x = dates,
+            y = rbf_svr.predict(output_days),
+            mode='lines+markers',
+            name='data'
+        )
+    )
+
+    fig.update_layout(
+        title="Predicted Close Price of next " + str(n_days - 1) + " days",
+        xaxis_title="Date",
+        yaxis_title="Closed Price"
+    )
+
+    return fig
